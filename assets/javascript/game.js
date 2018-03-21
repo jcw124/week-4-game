36cd69de2b07501854dcea44c92ab1ff266c69c6
janell <janell.williams@ticketmaster.com>
2018-03-20 19:20:15 -0700

    setting variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,26 @@
 
 // set intital displays 
-$( document ).ready(function() {
-});
+// $( document ).ready(function() {
+// });
 
 //define variables
+
+//define random #  for starting num
 var numStart;
+//define variables for wins and loses
 var wins = 0;
 var losses =0;
+//define variable for the score
 var score=0;
+//define random vales for jewels
 var jwla; 
 var jwlb;
 var jwlc;
 var jwld;
-var usersjwl;
 
-document.onclick = function setupGame(){
-    //starting number numStart (array range 19-120)
+
+
+    // starting number numStart (array range 19-120)
     numStart = Math.floor(Math.random() * 120) + 19;
     console.log(numStart);
      //jewel variables   jwla jwlb jwlc jwlc (random  1-12)
@@ -23,29 +28,81 @@ document.onclick = function setupGame(){
     jwlb = Math.floor(Math.random() * 12) + 1;
     jwlc =  Math.floor(Math.random() * 12) + 1;
     jwld = Math.floor(Math.random() * 12) + 1;
+    console.log('test here')
     console.log("value A is " + jwla + " value B is " + jwlb + " value C is " + jwlc + " value D is " + jwld);
     $("#random").html(numStart);
-}
+    $("#score").html(score);
 
 
- // current wins   wins
- //current losses losses
- // total score    score
+
+// add clickable buttons and setting the value
+
+//look jquery.this.onclick
+
+
+
+    $("#jwla").on("click", function(e){
+        // e.preventDefault();
+      score = score + jwla;
+      console.log("jwla is" + jwla );
+      console.log(score);
+      $("#score").html(score);
+    });
+
+    $("#jwlb" ).click(function(){
+    console.log("jwlb is" + jwlb );
+    score = score + jwlb;
+    console.log(score);
+    $("#score").html(score);
+    });
+
+
+    $("#jwlc" ).click(function(){
+        console.log("jwlc is" + jwlc );
+        score = score + jwlc;
+        console.log(score);
+        $("#score").html(score);
+    });
+
+
+    $("#jwld" ).click(function(){
+        console.log("jwld is" + jwld );
+        score = score + jwld;
+        console.log(score);
+        $("#score").html(score);
+    });
+
+//if score is less than the starting number allow the user to continue
+//if numStart < score {
+// console.log (numStart + "is less than" + score);   
+//} else
+
+// if score matches the starting number  mark it as a win, and display a win
+//if numStart === score {
+  //  console.log (numStart + "equals" + score); 
+//} else
+
+
+// if score it greater than starting number mark it as a lose and display a loss
+//if numStart > score {
+ //   console.log (numStart + "is greater than" + score);
+//}
+
+
+//}
+
+
 
  //userscurrent jewel selection value jwlval
 
-
-//define random #  for starting num
-//define random vales for jewels
-
 //when user clicks on a jewel set that value to jewel selection variable
 //add currentl jewel selection value to score
-//check  if the score matches starting number
-// if score is less than the starting number allow the user to continue
-// if score matches the starting number  mark it as a win, and display a win
-// if score it greater than starting number mark it as a lose and display a loss
 
 
-//lwhen win or loss start a new game */}
+
+//when win or loss start a new game */}
 
 
+//use to apply css to a new div for a Win or a lose
+ // We then apply that CSS to our newDiv.
+ //newDiv.setAttribute("class", "fancy");
